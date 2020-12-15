820a22722724a8e5b360ba216d2a844b63314300
sgloba <>
2020-12-15 11:53:50 +0300

    auth ready

--- routes/auth.js
+++ routes/auth.js
@@ -40,7 +40,7 @@ router.post(
                         { session: false },
                         async (error) => {
                             if (error) return next(error);
-                            const body = { _id: user._id, email: user.email };
+                            const body = { _id: user._id, username: user.username };
                             const token = jwt.sign({ user: body }, 'TOP_SECRET');
                             return res.json(token);
                         }
